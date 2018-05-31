27e63178d68cb8191c0c90f96712853a6d8968cb
jidhuvijayan <39726746+jidhuvijayan@users.noreply.github.com>
2018-05-31 14:51:57 +0530

    Add files via upload

---
+++ alternateletters.py
@@ -0,0 +1,6 @@
+name= raw_input("name")
+for i in xrange(len(name)):
+	if (i%2 == 0):
+		print name[i]
+
+		
---
+++ multiplication table.py
@@ -0,0 +1,5 @@
+number=input("Enter the number: ")
+def mul(n):
+	for i in xrange(1,11):
+		print i," * ",number,"= ",number*i
+mul(number)
